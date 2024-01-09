b3b43eba801be10fd61ea39470d4ec0899eef652
Niterux <parkerdec@gmail.com>
2024-01-09 15:22:12 -0500

    remove mixin plugins

--- client/src/main/java/io/github/niterux/mcpatcherfabric/hdtextures/reInitializeInterface.java
+++
@@ -1,9 +0,0 @@
-package io.github.niterux.mcpatcherfabric.hdtextures;
-
-
-import net.minecraft.client.options.GameOptions;
-import net.minecraft.client.render.texture.TextureManager;
-
-public interface reInitializeInterface {
-	void mcpatcherfabric$reInitialize(GameOptions options, String fontPath, TextureManager textureManager);
-}
